bd85bceb2fb6ecf7f27ce4f10141241db2b55107
Peter Prabu <peter.p@westagilelabs.com>
2023-02-13 16:12:59 +0530

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: 'peterprabu@test.com', password: 'password', role: 0)
+User.create!(email: 'rohitsharma@test.com', password: 'password', role: 1)
